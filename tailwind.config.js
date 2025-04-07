85b94cf38d7cbe982c5b27291833fdaa412c63c5
Akash Raikwar <akashraikwar763@gmail.com>
2025-04-07 03:26:27 -0400

    fix: added vercel.json for React Router SPA support

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 };
